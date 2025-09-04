ecdbc53ee77a5200fbbcd43a64aba8c9ca3a106a
jathvl <230452033+jathvl@users.noreply.github.com>
2025-09-04 23:24:47 +0200

    Initial commit (new spring boot app)

---
+++ src/main/java/no/jathvl/dat250experiment/Dat250ExperimentApplication.java
@@ -0,0 +1,13 @@
+package no.jathvl.dat250experiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dat250ExperimentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dat250ExperimentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/no/jathvl/dat250experiment/Dat250ExperimentApplicationTests.java
@@ -0,0 +1,13 @@
+package no.jathvl.dat250experiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dat250ExperimentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
